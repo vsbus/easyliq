d30cc1d26368732d5573d96dade6c67f14ceb3d2
Mariia <marianbarinova@gmail.com>
2013-10-30 17:55:51 -0700

    bug fix: copy calculation option ( was seen for density Calc)

--- EasyLiquidSeparation/war/assets/js/modules/module.js
+++ EasyLiquidSeparation/war/assets/js/modules/module.js
@@ -83,6 +83,9 @@ function Module(name, combos, parameters, groups, parameters_meta, groups_meta,
         var obj = new this.constructor();
         obj.id = null;
         obj.calculatedGroup = this.calculatedGroup;
+        for (i = 0; i < this.combos.length; i++) {
+            obj.combos[i].currentValue = this.combos[i].currentValue;
+        }
         obj.control = null;
 
         for (var parameter in obj.parameters_meta) {
